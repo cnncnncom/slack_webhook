903020db5dbc544ac6417793d5bfcc634bed4e37
cnncnncom <cnncnncom84@gmail.com>
2025-02-21 16:44:08 +0900

    last

--- Webhook.java
+++ Webhook.java
@@ -3,26 +3,24 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class Solution02 {
+public class Webhook {
     public static void main(String[] args) {
         String prompt = System.getenv("LLM_PROMPT");
-        String llmResult = useLLM(prompt); // 환경변수화
+        String llmResult = useLLM(prompt);
         System.out.println("llmResult = " + llmResult);
-
         String template = System.getenv("LLM2_IMAGE_TEMPLATE");
         String imagePrompt = template.formatted(llmResult);
         System.out.println("imagePrompt = " + imagePrompt);
         String llmImageResult = useLLMForImage(imagePrompt);
-        System.out.println("llmImageResult = " + llmImageResult); 
+        System.out.println("llmImageResult = " + llmImageResult);
         String title = System.getenv("SLACK_WEBHOOK_TITLE");
         sendSlackMessage(title, llmResult, llmImageResult);
     }
 
     public static String useLLMForImage(String prompt) {
-  
-        String apiUrl = System.getenv("LLM2_API_URL"); // 환경변수로 관리
-        String apiKey = System.getenv("LLM2_API_KEY"); // 환경변수로 관리
-        String model = System.getenv("LLM2_MODEL"); // 환경변수로 관리
+        String apiUrl = System.getenv("LLM2_API_URL");
+        String apiKey = System.getenv("LLM2_API_KEY");
+        String model = System.getenv("LLM2_MODEL");
         String payload = """
                 {
                   "prompt": "%s",
@@ -32,36 +30,30 @@ public class Solution02 {
                   "steps": 4,
                   "n": 1
                 }
-                """.formatted(prompt, model); // 대부분 JSON 파싱 문제는 , 문제!
-        HttpClient client = HttpClient.newHttpClient(); // 새롭게 요청할 클라이언트 생성
+                """.formatted(prompt, model);
+        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl)) // URL을 통해서 어디로 요청을 보내는지 결정
+                .uri(URI.create(apiUrl))
                 .header("Content-Type", "application/json")
                 .header("Authorization", "Bearer " + apiKey)
                 .POST(HttpRequest.BodyPublishers.ofString(payload))
-                .build(); // 핵심
-        String result = null; // return을 하려면 일단은 할당이 되긴 해야함
-        try { // try
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString());
+                .build();
+        String result = null;
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println("response.statusCode() = " + response.statusCode());
             System.out.println("response.body() = " + response.body());
-           
-            result = response.body()
-                    .split("url\": \"")[1]
-                    .split("\",")[0];
-        } catch (Exception e) { // catch exception e
+            result = response.body().split("url\": \"")[1].split("\",")[0];
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return result; // 앞뒤를 자르고 우리에게 필요한 내용만 리턴
+        return result;
     }
 
     public static String useLLM(String prompt) {
-      
-        String apiUrl = System.getenv("LLM_API_URL"); // 환경변수로 관리
-        String apiKey = System.getenv("LLM_API_KEY"); // 환경변수로 관리
-        String model = System.getenv("LLM_MODEL"); // 환경변수로 관리
-
+        String apiUrl = System.getenv("LLM_API_URL");
+        String apiKey = System.getenv("LLM_API_KEY");
+        String model = System.getenv("LLM_MODEL");
         String payload = """
                 {
                   "messages": [
@@ -73,34 +65,27 @@ public class Solution02 {
                   "model": "%s"
                 }
                 """.formatted(prompt, model);
-        HttpClient client = HttpClient.newHttpClient(); // 새롭게 요청할 클라이언트 생성
+        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl)) // URL을 통해서 어디로 요청을 보내는지 결정
+                .uri(URI.create(apiUrl))
                 .header("Content-Type", "application/json")
                 .header("Authorization", "Bearer " + apiKey)
                 .POST(HttpRequest.BodyPublishers.ofString(payload))
-                .build(); // 핵심
-        String result = null; // return을 하려면 일단은 할당이 되긴 해야함
-        try { // try
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString());
+                .build();
+        String result = null;
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println("response.statusCode() = " + response.statusCode());
             System.out.println("response.body() = " + response.body());
-           
-            result = response.body()
-                    .split("\"content\":\"")[1]
-                    .split("\"},\"logprobs\"")[0]; // 글씨 패턴 -> 스페이스나 escape 처리... 오타... 있음.
-        
-        } catch (Exception e) { // catch exception e
+            result = response.body().split("\"content\":\"")[1].split("\"},\"logprobs\"")[0];
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        return result; 
+        return result;
     }
 
     public static void sendSlackMessage(String title, String text, String imageUrl) {
-        String slackUrl = System.getenv("SLACK_WEBHOOK_URL"); // 환경변수로 관리
-  
+        String slackUrl = System.getenv("SLACK_WEBHOOK_URL");
         String payload = """
                     {"attachments": [{
                         "title": "%s",
@@ -108,22 +93,17 @@ public class Solution02 {
                         "image_url": "%s"
                     }]}
                 """.formatted(title, text, imageUrl);
-        HttpClient client = HttpClient.newHttpClient(); 
-      
+        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                
-                .uri(URI.create(slackUrl)) 
+                .uri(URI.create(slackUrl))
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(payload))
-                .build(); // 핵심
-
-        
-        try { // try
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString());
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println("response.statusCode() = " + response.statusCode());
             System.out.println("response.body() = " + response.body());
-        } catch (Exception e) { // catch exception e
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
